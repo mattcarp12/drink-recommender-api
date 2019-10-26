20c98b1366582b92ef0a219816ad69e6f1dfe05a
mattcarp12 <mattcarp88@gmail.com>
2019-10-26 13:04:37 -0400

    update

---
+++ Scripts/Script.sql
@@ -0,0 +1,12 @@
+insert into questions values ('hotdog', 'Is a hotdog a sandwich or taco?');
+insert into questions values ('gender', 'What is your gender?');
+
+insert into question_choices(choice, question_name) values ('Sandwich', 'hotdog');
+insert into question_choices(choice, question_name) values ('Taco', 'hotdog');
+insert into question_choices(choice, question_name) values ('Male', 'gender');
+insert into question_choices(choice, question_name) values ('Feale', 'gender');
+insert into question_choices(choice, question_name) values ('Other', 'gender');
+
+
+select * from questions;
+select * from question_choices;
--- src/main/java/org/matt/drinkrecommenderapi/DrinkRecommenderApiApplication.java
+++ src/main/java/org/matt/drinkrecommenderapi/DrinkRecommenderApiApplication.java
@@ -1,9 +1,5 @@
 package org.matt.drinkrecommenderapi;
 
-import java.net.URI;
-import java.net.URISyntaxException;
-
-import org.apache.commons.dbcp.BasicDataSource;
 import org.matt.drinkrecommenderapi.evaluator.DrinkModelEvaluator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -20,22 +16,5 @@ public class DrinkRecommenderApiApplication {
     static DrinkModelEvaluator getDrinkModelEvaluator() throws Exception {
         return new DrinkModelEvaluator();
     }
-
-	/*
-	 * @Bean public BasicDataSource dataSource() throws URISyntaxException { String
-	 * database_url = System.getenv("DATABASE_URL") == null ?
-	 * "postgres://postgres:postgres@localhost:5432/postgres" :
-	 * System.getenv("DATABASE_URL"); URI dbUri = new URI(database_url);
-	 * 
-	 * String username = dbUri.getUserInfo().split(":")[0]; String password =
-	 * dbUri.getUserInfo().split(":")[1]; String dbUrl = "jdbc:postgresql://" +
-	 * dbUri.getHost() + dbUri.getPath();
-	 * 
-	 * BasicDataSource basicDataSource = new BasicDataSource();
-	 * basicDataSource.setUrl(dbUrl); basicDataSource.setUsername(username);
-	 * basicDataSource.setPassword(password);
-	 * 
-	 * return basicDataSource; }
-	 */
 
 }
