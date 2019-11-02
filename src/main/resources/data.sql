e1f36c8e7aa851cb6bcb76135089dbb782212c19
mattcarp12 <mattcarp88@gmail.com>
2019-11-02 10:33:29 -0400

    update

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -3,8 +3,8 @@ insert into questions values ('tp', 'Toilet paper: over or under?');
 
 insert into question_choices(choice, question_name) values ('Sandwich', 'hotdog');
 insert into question_choices(choice, question_name) values ('Taco', 'hotdog');
-insert into question_choices(choice, question_name) values ('Over', 'tp')
-insert into question_choices(choice, question_name) values ('Under', 'tp')
+insert into question_choices(choice, question_name) values ('Over', 'tp');
+insert into question_choices(choice, question_name) values ('Under', 'tp');
 
 insert into drink values ('water', 'www.images.com/water.jpg');
 insert into drink values ('beer', 'www.images.com/beer.jpg');
