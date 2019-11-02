37d269ea53ce4237716ec7df50e9eb7fb2dcda18
mattcarp12 <mattcarp88@gmail.com>
2019-11-02 10:09:59 -0400

    update

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -6,8 +6,38 @@ insert into question_choices(choice, question_name) values ('Taco', 'hotdog');
 insert into question_choices(choice, question_name) values ('Over', 'tp')
 insert into question_choices(choice, question_name) values ('Under', 'tp')
 
-insert into drink values ('water', 'www.images.com/water.jpg')
-insert into drink values ('beer', 'www.images.com/beer.jpg')
-insert into drink values ('soda', 'www.images.com/soda.jpg')
-insert into drink values ('wine', 'www.images.com/wine.jpg')
+insert into drink values ('water', 'www.images.com/water.jpg');
+insert into drink values ('beer', 'www.images.com/beer.jpg');
+insert into drink values ('soda', 'www.images.com/soda.jpg');
+insert into drink values ('wine', 'www.images.com/wine.jpg');
+
+
+drop table if exists model_training_data;
+
+create table model_training_data (
+	id serial primary key,
+	drink varchar (50) not null,
+	hotdog varchar (50) not null,
+	tp varchar (50) not null
+);
+
+insert into model_training_data (drink, hotdog, tp) values ('beer', 'Sandwich', 'Over');
+insert into model_training_data (drink, hotdog, tp) values ('beer', 'Taco', 'Over');
+insert into model_training_data (drink, hotdog, tp) values ('beer', 'Sandwich', 'Under');
+insert into model_training_data (drink, hotdog, tp) values ('beer', 'Sandwich', 'Over');
+insert into model_training_data (drink, hotdog, tp) values ('water', 'Sandwich', 'Under');
+insert into model_training_data (drink, hotdog, tp) values ('water', 'Taco', 'Under');
+insert into model_training_data (drink, hotdog, tp) values ('water', 'Taco', 'Under');
+insert into model_training_data (drink, hotdog, tp) values ('water', 'Sandwich', 'Over');
+insert into model_training_data (drink, hotdog, tp) values ('soda', 'Taco', 'Over');
+insert into model_training_data (drink, hotdog, tp) values ('soda', 'Taco', 'Over');
+insert into model_training_data (drink, hotdog, tp) values ('soda', 'Taco', 'Under');
+insert into model_training_data (drink, hotdog, tp) values ('soda', 'Sandwich', 'Under');
+insert into model_training_data (drink, hotdog, tp) values ('wine', 'Taco', 'Under');
+insert into model_training_data (drink, hotdog, tp) values ('wine', 'Taco', 'Under');
+insert into model_training_data (drink, hotdog, tp) values ('wine', 'Taco', 'Over');
+insert into model_training_data (drink, hotdog, tp) values ('wine', 'Sandwich', 'Over');
+
+
+
 
