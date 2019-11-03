c9d4c04c1695bfc3560ebcc07c9e32d4029fec05
mattcarp12 <mattcarp88@gmail.com>
2019-11-03 07:25:55 -0500

    update

--- model-trainer/app.py
+++ model-trainer/app.py
@@ -7,21 +7,59 @@ from sklearn.naive_bayes import GaussianNB
 from sklearn.preprocessing import LabelBinarizer
 from sklearn_pandas import DataFrameMapper
 import sqlalchemy as db
+import logging
 
 
 def pub(myredis):
+    logging.info("Publishing on transferModel")
     myredis.publish('transferModel', 'transferModel')
 
-
-# Subscribe to messages that are sent from Spring Boot app 
-# When receive a message, invoke train_model()
 def sub(myredis, name):
     pubsub = myredis.pubsub()
     pubsub.subscribe(['trainModel'])
     for item in pubsub.listen():
-        train_model()
+        train_model('LIVE')
 
-def train_model():
+def get_sample_data(con):
+    data = pd.read_sql("select * from sample_training_data"
+                       , con = con
+                       , index_col="id")
+    
+    pipeline = PMMLPipeline([
+            ("transformation", DataFrameMapper([
+                (["hotdog"], [CategoricalDomain(), LabelBinarizer()]),
+                (["tp"], [CategoricalDomain(), LabelBinarizer()])
+            ])),
+            ("classifier", GaussianNB())
+        ])
+    return data, pipeline
+    
+def get_training_data(con):
+    data = pd.read_sql("select * from user_responses"
+                       , con = con
+                       , index_col="id")
+    
+    data.pivot(index = 'session_id', colums = 'question_name', values = 'question_choice')
+    
+    logging.info(data)
+    
+    pipeline = PMMLPipeline([
+            ("transformation", DataFrameMapper([
+                (["hotdog"], [CategoricalDomain(), LabelBinarizer()]),
+                (["tp"], [CategoricalDomain(), LabelBinarizer()]),
+                (["personality"], [CategoricalDomain(), LabelBinarizer()]),
+                (["dayofweek"], [CategoricalDomain(), LabelBinarizer()])
+            ])),
+            ("classifier", GaussianNB())
+        ])
+    return data, pipeline
+    
+    
+
+def train_model(dataType):
+    
+    from sklearn2pmml import PMMLPipeline
+    from sklearn2pmml.decoration import CategoricalDomain
     
     try:
         myredis
@@ -32,29 +70,19 @@ def train_model():
     DATABASE_URL = os.environ.get('DATABASE_URL') or "postgresql://postgres:postgres@localhost:5432/postgres"
     con = db.create_engine(DATABASE_URL)
 
-    #read data from postgres
-    data = pd.read_sql("select * from model_training_data"
-                       , con = con
-                       , index_col="id")
+    # Get data and pipeline
+    data, pipeline = get_training_data(con) if dataType == 'LIVE' else get_sample_data(con)
 
-    # create pipeline and fit model
-    from sklearn2pmml import PMMLPipeline
-    from sklearn2pmml.decoration import CategoricalDomain
     X = data[data.columns.difference(["drink"])]
     y = data["drink"]
 
-    pipeline = PMMLPipeline([
-            ("transformation", DataFrameMapper([
-                (["hotdog"], [CategoricalDomain(), LabelBinarizer()]),
-                (["tp"], [CategoricalDomain(), LabelBinarizer()])
-            ])),
-            ("classifier", GaussianNB())
-        ])
+    logging.info("Training model...")
     pipeline.fit(X, y)
 
     # save model as pmml file then put to redis
     from sklearn2pmml import sklearn2pmml
     sklearn2pmml(pipeline, "model.pmml")
+    logging.info("Saving trained model to redis under key: 'MODEL'")
     myredis.set(name = "MODEL", value = open("model.pmml", "r").read())
     
     # notify spring boot app the model is ready
@@ -66,6 +94,5 @@ def train_model():
 
 if __name__ == '__main__':
     myredis = redis.from_url(os.environ.get("REDIS_URL")) if os.environ.get("REDIS_URL") else redis.Redis()
-    #Thread(target=pub, args=(myredis,)).start()
     Thread(target=sub, args=(myredis,'reader1')).start()
 
--- src/main/java/org/matt/drinkrecommenderapi/model/dto/UserResponseDTO.java
+++ src/main/java/org/matt/drinkrecommenderapi/model/dto/UserResponseDTO.java
@@ -6,7 +6,6 @@ import java.util.Map;
 
 @Data
 public class UserResponseDTO {
-    //int surveyId;
     String user;
     String drink;
     Map<String, String> answers;
